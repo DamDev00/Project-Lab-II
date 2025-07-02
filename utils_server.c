d3b8f19fcbaaaf496233ef4b3b10d651ebf4e514
Damiano <119160025+DamDev00@users.noreply.github.com>
2025-07-02 12:32:23 +0200

    Update utils_server.c

--- utils_server.c
+++ utils_server.c
@@ -68,7 +68,14 @@ void free_waiting_queue(waiting_queue_t** waiting_queue, int num){
 }
 
 int barrier_rescuers(emergency_id_t* current ,atomic_int* count, atomic_int* tot_rescuers_required, mtx_t* mtx, cnd_t* cnd, bool is_active) {
+
 	mtx_lock(mtx);
+
+    /*
+        is_active verifica se la message queue è ancora operativa, se non lo è
+        risveglia tutti i soccorritori che erano nello stato wait per poi terminare
+        la loro esecuzione dato che dopo la funzione c'è 'if(!MESSAGE_QUEUE_ACTIVE) break;'
+    */
 
     if(!is_active){
         cnd_broadcast(cnd);
@@ -76,8 +83,15 @@ int barrier_rescuers(emergency_id_t* current ,atomic_int* count, atomic_int* tot
         return 0;
     }
 
+    // Aggiorno il numero di soccorritori che sono arrivati
+
     (*count)++;
 	
+    /*
+        se sono arrivati tutti i soccorritori, l'ultimo che è arrivato risveglia tutti gli altri
+        modificando lo stato dell'emergenza riportandolo sul file.log
+    */
+
 	if (*count == *tot_rescuers_required) {
 		*count = 0;
 		cnd_broadcast(cnd);
@@ -121,28 +135,43 @@ int distance_manhattan(int x1, int x2, int y1, int y2, int speed){
 
 int rescuers_is_avalaible(rescuer_digital_twin_t** rd_twins, int num_twins, rescuer_request_t* requests, int req_number, char* desc){
 
-    bool whitout = false;
+    /*
+        verifico che tutti i tipi di soccorritori richiesti per l'emergenza ci siano
+    */
+
+    bool mancamenti = false;
     for(int i = 0; req_number > i; i++){
         if(requests[i].type == NULL){
             printf("non è possibile soddisfare l'emergenza: %s per mancanza di soccorritori necessari\n", desc);
             return -1;
         }
         int counter_resc = 0;
+
+        /*
+            verifico quanti soccorritori ci sono per un determinato tipo: se ci sono tutti mi fermo prima,
+            se non c'è n'è nessuno ritorno -1 segnalando che l'emergenza non è fattibile
+        */
+
         for(int j = 0; num_twins > j; j++){
             if(strcmp(rd_twins[j]->rescuer->rescuer_type_name, requests[i].type->rescuer_type_name) == 0) counter_resc++;
             if(counter_resc == requests[i].required_count) break;
         }
         if(counter_resc == 0) return -1;
         else if(counter_resc < requests[i].required_count){
-            whitout = true;
+            mancamenti = true;
         } 
     }
 
-    return (whitout) ? 0 : 1;
+    return (mancamenti) ? 0 : 1;
 
 }
 
 int compare(const void *a, const void *b) {
+
+    /*
+        compare che utilizzo per ordinare la waiting queue in base alla priorità
+    */
+
     waiting_queue_t* em1 = *(waiting_queue_t**)a;
     waiting_queue_t* em2 = *(waiting_queue_t**)b;
 
@@ -152,6 +181,11 @@ int compare(const void *a, const void *b) {
 }
 
 void add_waiting_queue(emergency_id_t* id, waiting_queue_t*** waiting_queue, int* waiting_queue_len){
+    
+    /*
+        se la coda è vuota, alloco memoria. Se non è vuota, prima di ri-allocare memoria
+        verifico che l'emergenza non sia in coda
+    */
     
     if (*waiting_queue == NULL){
         *waiting_queue = (waiting_queue_t**)malloc(sizeof(waiting_queue_t*));
@@ -174,11 +208,15 @@ void add_waiting_queue(emergency_id_t* id, waiting_queue_t*** waiting_queue, int
         *waiting_queue = tmp;
     }
 
+    // alloco memoria per l'emergenza corrente da aggiungere in coda
+
     (*waiting_queue)[*waiting_queue_len] = malloc(sizeof(waiting_queue_t));
     if ((*waiting_queue)[*waiting_queue_len] == NULL) {
         printf("{type error: MALLOC_ERROR; line: %d; file: %s}\n", __LINE__, __FILE__);
         exit(MALLOC_ERROR);
     }
+
+    // alloco memoria per la descrizione dell'emergenza
 
     (*waiting_queue)[*waiting_queue_len]->desc = malloc(sizeof(char) * LENGTH_LINE);
     if ((*waiting_queue)[*waiting_queue_len]->desc == NULL) {
@@ -186,17 +224,28 @@ void add_waiting_queue(emergency_id_t* id, waiting_queue_t*** waiting_queue, int
         exit(MALLOC_ERROR);
     }
 
+    // inizializzo i parametri dell'emergenza corrente aggiunta in coda
+
     strcpy((*waiting_queue)[*waiting_queue_len]->desc, id->emergency->type->emergency_desc);
     (*waiting_queue)[*waiting_queue_len]->id = id->id;
     (*waiting_queue)[*waiting_queue_len]->priority = id->emergency->type->priority;
 
     (*waiting_queue_len)++;
 
+    // ordino la coda in base alla priorità
+
     qsort(*waiting_queue, *waiting_queue_len, sizeof(waiting_queue_t*), compare);
+
+    return;
 }
 
 void remove_from_waiting_queue(emergency_id_t* emergency, waiting_queue_t*** waiting_queue, int* waiting_queue_len) {
+
+    // verifico che la coda non sia vuota
+
     if (*waiting_queue == NULL || *waiting_queue_len <= 0) return;
+
+    // controllo subito se la coda ha solamente un emergenza ed è quella passata per parametro
 
     if (*waiting_queue_len == 1 && emergency->id == (*waiting_queue)[0]->id) {
         printf("[(%d,%s) ELIMINATA DALLA CODA DI ATTESA]\n", emergency->id, (*waiting_queue)[0]->desc);
@@ -208,6 +257,11 @@ void remove_from_waiting_queue(emergency_id_t* emergency, waiting_queue_t*** wai
         return;
     }
 
+    /*
+        scorro la coda per trovare l'id dell'emergenza da eliminare
+        salvando l'indice
+    */
+
     int index = -1;
     for (int i = 0; i < *waiting_queue_len; i++) {
         if ((*waiting_queue)[i]->id == emergency->id) {
@@ -216,16 +270,30 @@ void remove_from_waiting_queue(emergency_id_t* emergency, waiting_queue_t*** wai
         }
     }
 
+    // se rimane -1, l'emergenza non è in coda d'attesa
+
     if (index == -1) {
         printf("[L'EMERGENZA (%d,%s) NON ERA IN CODA D'ATTESA]\n", emergency->id, emergency->emergency->type->emergency_desc);
         return;
     }
 
+    /*
+        mi salvo il nome dell'emergenza per segnalare, alla fine della funzione, che l'emergenza è
+        stata rimossa dalla coda d'attesa
+    */
+
     char temp_desc[LENGTH_LINE];
     strcpy(temp_desc, (*waiting_queue)[index]->desc);
 
+    // libero la memoria
+
     free((*waiting_queue)[index]->desc);
     free((*waiting_queue)[index]);
+
+    /*
+        sovrascrivo l'emergenza i-esima con la i+1-esima per poter eliminare l'ultimo elemento
+        diminuendo la grandezza della coda
+    */
 
     for (int i = index; i < *waiting_queue_len - 1; i++) {
         (*waiting_queue)[i] = (*waiting_queue)[i + 1];
@@ -234,9 +302,10 @@ void remove_from_waiting_queue(emergency_id_t* emergency, waiting_queue_t*** wai
     (*waiting_queue)[*waiting_queue_len - 1] = NULL;
     (*waiting_queue_len)--;
 
-    // Ridimensiona la coda
+    // ridimensiono la coda d'attesa di -1
+
     if (*waiting_queue_len > 0) {
-        waiting_queue_t** tmp = realloc(*waiting_queue, sizeof(waiting_queue_t*) * (*waiting_queue_len));
+        waiting_queue_t** tmp = realloc(*waiting_queue, sizeof(waiting_queue_t*)*(*waiting_queue_len));
         if (tmp != NULL) {
             *waiting_queue = tmp;
         }
@@ -246,10 +315,14 @@ void remove_from_waiting_queue(emergency_id_t* emergency, waiting_queue_t*** wai
     }
 
     printf("[(%d,%s) ELIMINATA DALLA CODA DI ATTESA]\n", emergency->id, temp_desc);
+    return;
+
 }
 
 
-emergency_t* set_new_emergency(params_handler_emergency_t* params_emergency){
+emergency_t* set_new_emergency(params_handler_emergency_t* params_emergency, int* twins){
+
+    // alloco memoria per l'output (emergency)
 
     emergency_t* emergency = (emergency_t*)malloc(sizeof(emergency_t));
     if(emergency == NULL){
@@ -257,11 +330,16 @@ emergency_t* set_new_emergency(params_handler_emergency_t* params_emergency){
         exit(MALLOC_ERROR);
     }
 
-    int index = params_emergency->index_emergencies;
-    int x = params_emergency->x;
-    int y = params_emergency->y;
+    /*
+        inizializzo tutti i campi per il tipo emergency_t
+    */
 
+    int index = params_emergency->index_emergencies;
+    emergency->x = params_emergency->x;
+    emergency->y = params_emergency->y;
     emergency->time = params_emergency->timestamp;
+
+    
     emergency->type = (emergency_type_t*)malloc(sizeof(emergency_type_t));
 
     if(emergency->type == NULL){
@@ -269,6 +347,7 @@ emergency_t* set_new_emergency(params_handler_emergency_t* params_emergency){
         exit(MALLOC_ERROR);
     }
     emergency->type->emergency_desc = (char*)malloc(sizeof(char)*LENGTH_LINE);
+
     if(emergency->type->emergency_desc == NULL){
         printf("{type error: MALLOC_ERROR; line: %d; file: %s}\n",__LINE__,__FILE__);
         exit(MALLOC_ERROR);
@@ -285,6 +364,10 @@ emergency_t* set_new_emergency(params_handler_emergency_t* params_emergency){
         exit(MALLOC_ERROR);
     }
 
+    /*
+        imposto i soccorritori necessari richiesti
+    */
+
     for(int i = 0; emergency->type->rescuers_req_number > i; i++){
         if(params_emergency->emergency_avalaible[index].rescuers[i].type != NULL){
             emergency->type->rescuers[i].required_count = params_emergency->emergency_avalaible[index].rescuers[i].required_count;
@@ -293,8 +376,48 @@ emergency_t* set_new_emergency(params_handler_emergency_t* params_emergency){
         }
     }
 
-    emergency->x = x;
-    emergency->y = y;
+    // alloco memoria per i gemelli digitali
+
+    emergency->rescuers_dt = (rescuer_digital_twin_t**)malloc(sizeof(rescuer_digital_twin_t*));
+    if(emergency->rescuers_dt == NULL){
+        printf("{type error: MALLOC_ERROR; line: %d; file: %s}\n",__LINE__,__FILE__);
+        exit(MALLOC_ERROR);
+    }
+
+    /*
+        in questo ciclo in sostanza, nell'array emergency->rescuers_dt, vado ad aggiungere più possibili
+        soccorritori necessari per ogni tipo, cosa da avere una panoramica al 100% per capire chi posso avviare
+        e chi no. Ad esempio, se ho [Allagamento][<int>]Pompieri: 4,5 e nel file.rescuers ho [Pompieri][10] non vado
+        ad assergnare solamente 4 istanze, ma tutte e 10 le disponibili.
+    */
+    
+
+    for(int i = 0; emergency->type->rescuers_req_number > i; i++){
+        if(params_emergency->emergency_avalaible[index].rescuers[i].type != NULL){
+
+            // i-esimo soccorritore richiesto
+
+            rescuer_request_t req = params_emergency->emergency_avalaible[index].rescuers[i];
+
+            for(int j = 0; params_emergency->params->rp_rescuers->num_twins > j; j++){
+                // verifico se è lui attraverso il nome
+
+                if(strcmp(req.type->rescuer_type_name, params_emergency->params->rp_rescuers->rd_twins[j]->rescuer->rescuer_type_name) == 0){
+                    
+                    // lo aggiungo nell'array e ri-alloco memoria di +1
+                    
+                    emergency->rescuers_dt[*twins] = params_emergency->params->rp_rescuers->rd_twins[j];
+                    (*twins)++;
+                    rescuer_digital_twin_t** tmp = realloc(emergency->rescuers_dt, sizeof(rescuer_digital_twin_t*)*((*twins)+1));
+                    if(tmp == NULL){
+                        printf("{type error: MALLOC_ERROR; line: %d; file: %s}\n",__LINE__,__FILE__);
+                        exit(MALLOC_ERROR);
+                    }
+                    emergency->rescuers_dt = tmp;
+                }
+            }
+        }
+    }
 
     emergency->status = ASSIGNED;
     
@@ -305,7 +428,12 @@ emergency_t* set_new_emergency(params_handler_emergency_t* params_emergency){
 
 emergency_id_t* add_emergency(int* id, emergency_t* emergency, emergency_id_t*** queue_emergencies){
 
+    /*
+        inizializzo il campo 'emergency', l'id, e lo aggiungo nell'array delle emergenze
+    */
+
     emergency_id_t* new_emergency = (emergency_id_t*)malloc(sizeof(emergency_id_t));
+    
     if(new_emergency == NULL){
         printf("{type error: MALLOC_ERROR; line: %d; file: %s}\n",__LINE__,__FILE__);
         exit(MALLOC_ERROR);
@@ -334,6 +462,14 @@ emergency_id_t* add_emergency(int* id, emergency_t* emergency, emergency_id_t***
 
 int is_valid_request(emergency_request_t* request, int width, int height, char* problem, int* index_type_emergency, emergency_type_t* emergency_avalaible, int num_emergency_avalaible){
 
+    /*
+        qui verifico che l'emergenza sia valida per: nome e coordinate;
+        per ogni tipo di problema, assegno alla variabile 'problem' una stringa
+        che descrive il tipo di problema.
+
+        La variabile is_valid determina il ritorno della funzione
+    */
+    
     bool is_valid = true;
 
     if(request == NULL) {
@@ -346,13 +482,19 @@ int is_valid_request(emergency_request_t* request, int width, int height, char* 
         } else strcat(problem, "overflow coordinate;");
     }
 
-
+    if(!is_valid) return 0;
     
     for(int i = 0; num_emergency_avalaible > i; i++){
+
+        // se l'emergenza ci sta, allora fermo il ciclo e mi salvo l'indice
         if(strcmp(request->emergency_name, emergency_avalaible[i].emergency_desc) == 0){
             *index_type_emergency = i;
             break;
         }
+        /*
+            se sono arrivato qui, nell'ultima iterazione del ciclo, significa che non è stata trovata
+            l'emergenza da soddisfare, per cui salvo il tipo di problema e imposto la variabile a false
+        */
         else if(i == num_emergency_avalaible - 1){
             if(is_valid){
                 strcpy(problem, "questa emergenza non è presente nel file emergency.conf;");
@@ -368,19 +510,46 @@ int is_valid_request(emergency_request_t* request, int width, int height, char* 
 
 int check_priority_waiting_queue(int current_index, waiting_queue_t*** waiting_queue, int waiting_queue_len, emergency_id_t*** queue_emergencies){
 
+    /*
+        verifico che la coda d'attesa abbia più di un elemento e che l'indice corrente non 
+        sia maggiore o uguale alla lunghezza della coda d'attesa
+    */
+
     if(current_index >= waiting_queue_len || !(waiting_queue_len > 1)) return -1;
+    
+    // verifico se esiste l'emergenza nell'indice corrente
+    
     if((*waiting_queue)[current_index] == NULL) return -1;
+    
+    // mi salvo l'id dell'emergenza in attesa nell'indice corrente
+
     int id = (*waiting_queue)[current_index]->id;
+
+    // verifico che esista questo id nell'array globale delle emergenze
+
     if (!(*queue_emergencies)[id] || !(*queue_emergencies)[id]->emergency || !(*queue_emergencies)[id]->emergency->type) return -1;
 
+    // mi salvo la priorità dell'emergenza e il nome 
     int current_priority = (*queue_emergencies)[id]->emergency->type->priority;
+
+    /*
+        se ha priorità 0 significa che si trova in fondo alla coda, per cui non importa
+        preoccuparsi di altre emergenze con priorità 0, riparto direttamente dalla prima nella coda d'attesa
+    */
+
+    if(current_priority == 0) return -1;
+
     char curr_name[LENGTH_LINE];
     strcpy(curr_name, (*queue_emergencies)[id]->emergency->type->emergency_desc);
+   
+    /*
+        verifico che ci siaun emergenza diversa con priorità uguale
+    */
    
     for(int i = current_index; waiting_queue_len - 1 > i; i++){
         if(current_priority == (*queue_emergencies)[(*waiting_queue)[i+1]->id]->emergency->type->priority &&
         strcmp(curr_name, (*queue_emergencies)[(*waiting_queue)[i+1]->id]->emergency->type->emergency_desc) != 0){
-            printf("c'è un emergenza (%d,%s) che può essere avviata con priorità uguale\n",(*waiting_queue)[i+1]->id, (*waiting_queue)[i+1]->desc);
+            printf("[L'EMERGENZA (%d,%s) CON PRIORITA' UGUALE A QUELLA CORRENTE POTREBBE ESSERE AVVIATA]\n",(*waiting_queue)[i+1]->id, (*waiting_queue)[i+1]->desc);
             return i+1;
         }
     }
