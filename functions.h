595052eb6a53c14d4c098c2278b37a87da11fef2
Damiano <119160025+DamDev00@users.noreply.github.com>
2025-07-05 18:04:41 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -8,14 +8,23 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+// variabile che uso in diversi file.c per la lunghezza degli array, in particolare per stringhe/parser
+
 #define LENGTH_LINE 256
+
+// macro in cui definisco dei codici di errore in base all'operazione corrente
 #define MALLOC_ERROR 0
 #define FILE_ERROR 1
 #define REALLOC_ERROR 2
+
+// nomi dei file parser
+
 #define RESCUERS_FILENAME "rescuers.conf"
 #define EMERGENCY_FILENAME "emergency.conf"
 #define ENVIRONMENT_FILENAME "env.conf"
 #define LOG_FILENAME "events.log"
+
+// protezioni per le syscall
 
 #define SCALL_ERROR -1
 #define SCALL(r, c, e) do { if((r = c) == SCALL_ERROR) { perror(e); exit(EXIT_FAILURE); } } while(0)
